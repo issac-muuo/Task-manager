ff5eb4f026023f9b869f050c78d82a8cb29d7b9a
Isaac Muuo <isaacmuuo27@gmail.com>
2025-12-18 18:10:47 +0300

    added a render function

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 import { createStore } from "./data/store.js";
 
-const store = createStore({ task: [] });
+const store = createStore({ tasks: [] });
 document.get;
 const input = document.getElementById("task-input");
 const addBtn = document.getElementById("add-btn");
 const undoBtn = document.getElementById("undo-btn");
 const redoBtn = document.getElementById("redo-btn");
-const list=document.getElementById("task-list")
+const list = document.getElementById("task-list");
+
+function render() {
+  const { tasks } = store.getState;
+
+  list.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+    li.textContent = task.text;
+    list.appendChild(li);
+  });
+}
